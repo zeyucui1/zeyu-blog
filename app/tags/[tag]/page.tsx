37ef9001093d61cff3d08cef6f8f170116de0f12
zeyucui1 <zeyu.cui@student.uts.edu.au>
2024-07-23 16:00:48 +1000

    feat: create tag page

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -1,28 +1,35 @@
+import { getAllTags, getPostsByTagSlug, sortTagsByCount } from '@/lib/utils'
 import { posts } from '#site/content'
 import PostItem from '@/components/post-item'
-import { QueryPagination } from '@/components/query-pagination'
-import { Tag } from '@/components/tag'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { getAllTags, sortPosts, sortTagsByCount } from '@/lib/utils'
-export const metadata = {
-  title: 'Blog',
+import { Tag } from '@/components/tag'
+import { slug } from 'github-slugger'
+import { Metadata } from 'next'
+interface TagPageProps {
+  params: {
+    tag: string
+  }
 }
-const POSTS_PER_PAGE = 5
-interface BlogPageProps {
-  searchParams: {
-    page?: string
+export async function generateMetadata({
+  params,
+}: TagPageProps): Promise<Metadata> {
+  const { tag } = params
+  return {
+    title: tag,
+    description: `Posts on the topic of ${tag}`,
   }
 }
 
-export default async function BlogPage({ searchParams }: BlogPageProps) {
-  const currentPage = Number(searchParams?.page) || 1
-  const sortedPosts = sortPosts(posts.filter((post) => post.published))
-  const totalPages = Math.ceil(sortedPosts.length / POSTS_PER_PAGE)
-  const displayPosts = sortedPosts.slice(
-    POSTS_PER_PAGE * (currentPage - 1),
-    POSTS_PER_PAGE * currentPage
-  )
+export const generateStaticParams = () => {
+  const tags = getAllTags(posts);
+  const paths = Object.keys(tags).map((tag) => ({ tag: slug(tag) }));
+  return paths;
+};
+export default function TagPage({ params }: TagPageProps) {
+  const { tag } = params
+  const title = tag.split('-').join(' ')
 
+  const displayPosts = getPostsByTagSlug(posts, tag)
   const tags = getAllTags(posts)
   const sortedTags = sortTagsByCount(tags)
 
@@ -30,19 +37,18 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
     <div className="container max-w-4xl py-6 lg:py-10">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
-          <p className="text-xl text-muted-foreground">
-            My Learning note and thoughts in web development
-          </p>
+          <h1 className="inline-block font-black text-4xl lg:text-5xl capitalize">
+            {title}
+          </h1>
         </div>
       </div>
       <div className="grid grid-cols-12 gap-3 mt-8">
         <div className="col-span-12 col-start-1 sm:col-span-8">
-          <hr className="mt-8" />
-          {displayPosts.length > 0 ? (
+          <hr />
+          {displayPosts?.length > 0 ? (
             <ul className="flex flex-col">
               {displayPosts.map((post) => {
-                const { slug, title, description, date, tags } = post
+                const { slug, date, title, description, tags } = post
                 return (
                   <li key={slug}>
                     <PostItem
@@ -59,18 +65,14 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
           ) : (
             <p>Nothing to see here yet</p>
           )}
-          <QueryPagination
-            totalPages={totalPages}
-            className="justify-end mt-4"
-          />
         </div>
-        <Card className="col-span-12 row-start-3 h-fit sm:col-span-4 sm:col-start-9 sm:row-start-1 sm:ml-4">
+        <Card className="col-span-12 row-start-3 h-fit sm:col-span-4 sm:col-start-9 sm:row-start-1">
           <CardHeader>
             <CardTitle>Tags</CardTitle>
           </CardHeader>
           <CardContent className="flex flex-wrap gap-2">
-            {sortedTags?.map((tag) => (
-              <Tag tag={tag} key={tag} count={tags[tag]} />
+            {sortedTags?.map((t) => (
+              <Tag tag={t} key={t} count={tags[t]} current={slug(t) === tag} />
             ))}
           </CardContent>
         </Card>
--- components/tag.tsx
+++ components/tag.tsx
@@ -1,7 +1,33 @@
 import Link from 'next/link'
 import { slug } from 'github-slugger'
-
 import { badgeVariants } from './ui/badge'
+
+const glassColors = [
+  'bg-red-500 bg-opacity-30 backdrop-blur-md',
+  'bg-blue-500 bg-opacity-30 backdrop-blur-md',
+  'bg-green-500 bg-opacity-30 backdrop-blur-md',
+  'bg-yellow-500 bg-opacity-30 backdrop-blur-md',
+  'bg-purple-500 bg-opacity-30 backdrop-blur-md',
+  'bg-pink-500 bg-opacity-30 backdrop-blur-md',
+  'bg-teal-500 bg-opacity-30 backdrop-blur-md',
+  'bg-orange-500 bg-opacity-30 backdrop-blur-md',
+]
+
+function hashString(str: any) {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    const char = str.charCodeAt(i)
+    hash = (hash << 5) - hash + char
+    hash |= 0
+  }
+  return hash
+}
+
+function getColorForTag(tag: any) {
+  const index = Math.abs(hashString(tag)) % glassColors.length
+  return glassColors[index]
+}
+
 interface tagProps {
   tag: string
   current?: boolean
@@ -9,12 +35,14 @@ interface tagProps {
 }
 
 export function Tag({ tag, current, count }: tagProps) {
+  const colorClass = getColorForTag(tag)
+
   return (
     <Link
-      className={badgeVariants({
+      className={`${badgeVariants({
         variant: current ? 'default' : 'secondary',
         className: 'no-underline rounded-md',
-      })}
+      })} ${colorClass}`}
       href={`/tags/${slug(tag)}`}
     >
       {tag}
